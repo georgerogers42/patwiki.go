77bb727799b9f2ba197714ec1862da1bfcc6e4df
George Rogers <georgerogers42@aol.com>
2012-02-18 15:39:45 -0600

    foo

--- hello.go
+++ hello.go
@@ -8,10 +8,11 @@ import (
 )
 
 func main() {
-	port := flag.String("p","5000")
+	port := flag.String("p", "5000", "Port to run on")
+	flag.Parse()
 	m := pat.New()
 	m.Get("/hello/:name", http.HandlerFunc(hello))
-	http.ListenAndServe(":"+port, m)
+	http.ListenAndServe(":"+*port, m)
 }
 
 var helloTpl = template.Must(template.ParseFiles("hello.html"))
@@ -19,5 +20,5 @@ var helloTpl = template.Must(template.ParseFiles("hello.html"))
 func hello(w http.ResponseWriter, r *http.Request) {
 	// Path variable names are in the URL.Query() and start with ':'.
 	name := r.URL.Query().Get(":name")
-	helloTpl.Execute(w, map[string]string{"name":name})
+	helloTpl.Execute(w, map[string]string{"name": name})
 }
